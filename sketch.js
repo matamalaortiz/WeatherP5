98f0588afa89d42256aca0b2d5200a3ae2dd06bd
Alejandro Matamala <matamala@material.cl>
2016-10-19 23:14:24 -0400

    last commit

--- sketch.js
+++ sketch.js
@@ -1,87 +1,83 @@
-
 var weather;
-frio = "Brrr\nMUCHO\nFRIOOO\nABRIGATE";
-calor = "Today\nIt's\na Sunny\nDay";
+
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&APPID=7092c585ac798611e44cbb9dc55d4db9';
+var units = '&units=metric';
+
+var input;
+first = "Hi,\nHow is\nthe\nWeather?";
+frio = "Brrr,\nit's very\ncold, wrap\nyourself!";
+medium = "Mmmm, it\nis not \nthat bad.";
+calor = "It's great \nand \nwarm\nout there.";
 
 
-
-
-var nameCity;
-var select;
 
 function setup() {
   createCanvas(500,500);
-  text('Weather of:',20,20);
-  
-  sel = createSelect();
-  sel.position(20, 40);
-  sel.option('New York');
-  sel.option('Santiago');
-  sel.option('Lima');
-  sel.changed(mySelectEvent);
+  var button = select('#submit');
+  button.mousePressed(weatherAsk);
+	input = select('#city');
 
+} 
+  function weatherAsk() {
+    var url = api + input.value() + apiKey + units;
+    loadJSON(url, gotData);
+  }
+
+
+
+function gotData(data) {
+	weather = data;
 }
 
 function draw() {
-  background(123);
-  fill(255);
-  // get the humidity value out of the loaded JSON
-  var temp_max = weather.main.temp_max;
-  if ( temp_max < 20 ) {
+  background(255);
+  textStyle(BOLD);  
+  textSize(80);
+  textLeading(85);
+  text(first, 15,110 );
+  
+  if (weather) {
+	var temp_max = weather.main.temp_max;
+  var description = weather.weather[0].description;
+
+  if ( temp_max < 11 ) {
 
 
-    textSize(22);
-    textFont('Apercu-Bold');
-    textLeading(29);
-    text(frio , 0,130 );
-    
-    textFont('Apercu-Mono');
+    textSize(82);
+    textLeading(84);
+    text(frio , 15,140 );
     textSize(12);
-    text('Now is ' + temp_max + ' Celcius Degrees', 10, height-40);
+    text('Now is ' + temp_max + ' Celcius degrees' + " with " + description + '.', 10, height-40);
+		first = "";
+
+    
+  }  else if( temp_max > 11 && temp_max < 18  ) {
+
+
+    textSize(92);
+    textLeading(90);
+    text(medium , 15,140 );
+    
+    textSize(12);
+    text('Now is ' + temp_max + ' Celcius degrees' + " with " + description + '.', 10, height-40);
+		first = "";
 
   }else{
-      textSize(22);
-    textFont('Apercu-Bold');
-    textLeading(29);
-    text(calor, 0,130 );
+    textSize(92);
+    textLeading(90);
+    text(calor, 15,140 );
     
-    textFont('Apercu-Mono');
     textSize(12);
-    text('Now is ' + temp_max + ' Celcius Degrees', 10, height-40);
+    text('Now is ' + temp_max + ' Celcius degrees' + " with " + description + '.'  , 10, height-40);
+		first = "";
 
   }
-    text('Weather of:',20,20);
-  if ( sel.value == 'Santiago'){
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=Santiago' +
-   '&APPID=7092c585ac798611e44cbb9dc55d4db9&units=metric';
-    weather = loadJSON(url);
-
+    textStyle(BOLD);
+    text('The weather in ' + input.value() + ',',20,40);
   }
+  
+
 
 }
 
-function mySelectEvent() {
-  var item = sel.value();
-  background(200);
-    if (sel.value() == 'Santiago'){
-     var url = 'http://api.openweathermap.org/data/2.5/weather?q=Santiago' + 
-     '&APPID=7092c585ac798611e44cbb9dc55d4db9&units=metric';
-    weather = loadJSON(url);
-      }
-    if (sel.value() == 'Lima'){
-   var url = 'http://api.openweathermap.org/data/2.5/weather?q=Lima' + 
-   '&APPID=7092c585ac798611e44cbb9dc55d4db9&units=metric';
-  weather = loadJSON(url);
-    }
-  
-  println(sel.value());
-  
-}
-
-function preload() {
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q=NewYork' +
-   '&APPID=7092c585ac798611e44cbb9dc55d4db9&units=metric';
-  weather = loadJSON(url);
-  
-
-}
